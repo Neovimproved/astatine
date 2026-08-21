a7a8f4fc9ad584653c331ad21254a27714872bd1
Neovimproved <neovimproved@yahoo.com.(none)>
2026-08-21 19:00:20 +0100

    chore: add cross platform support to flake

--- flake.nix
+++ flake.nix
@@ -2,31 +2,50 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs =
     { self, nixpkgs }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      lib = nixpkgs.lib;
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
     in
     {
+      packages = lib.genAttrs supportedSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "Astatine";
+            version = "0.1.0";
+            cargoLock.lockFile = ./Cargo.lock;
+            src = pkgs.lib.cleanSource ./.;
+          };
+        }
+      );
 
-      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
-        pname = "Astatine";
-        version = "0.1.0";
-        cargoLock.lockFile = ./Cargo.lock;
-        src = pkgs.lib.cleanSource ./.;
+      devShells = lib.genAttrs supportedSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            inputsFrom = [ self.packages.${system}.default ];
 
-      };
-
-      devShells.${system}.default = pkgs.mkShell {
-        inputsFrom = [ self.packages.${system}.default ];
-
-        nativeBuildInputs = with pkgs; [
-          rust-analyzer
-        ];
-      };
+            nativeBuildInputs = with pkgs; [
+              rust-analyzer
+            ];
+          };
+        }
+      );
     };
 }
